16e8ec95c5ac27ac99107337fda4ad43815f32a3
stanley <stanley@zhihesoft.com>
2021-12-02 23:07:02 +0800

    version 1

---
+++ lib/index.ts
@@ -0,0 +1,123 @@
+import "reflect-metadata";
+import { container, InjectionToken } from "tsyringe";
+import { Express, Request, Router, Response } from "express";
+
+export function service<T>(path: string, token: InjectionToken<T>) {
+    return function (constructor: Function) {
+        let map: Map<string, any> = Reflect.getMetadata(service_router_token, constructor.prototype);
+        if (!map) {
+            map = new Map<string, any>();
+        }
+        map.set(path, container.resolve(token));
+        Reflect.defineMetadata(service_router_token, map, constructor.prototype);
+    }
+}
+
+export function api(path: string, security: boolean = true, method: string = "POST") {
+    return function (target: any, propertyKey: string) {
+        defineRequestMetadata(path, target, propertyKey, security, method);
+    };
+}
+
+export function register(app: Express, module: any, guardian: ConnectFunction) {
+    const map: Map<string, any> = Reflect.getMetadata(service_router_token, module.prototype);
+    if (!map) {
+        console.error("no router of service found in " + module);
+        return;
+    }
+    for (const item of map) {
+        const router = createRouter(item[1], guardian);
+        app.use(item[0], router);
+    }
+}
+
+function createRouter(handler: any, guardian: ConnectFunction): Router {
+    const router: Router = Router();
+    const map: Map<string, ServiceHandlerInfo> = Reflect.getMetadata(service_api_map, Object.getPrototypeOf(handler));
+    if (!map) {
+        console.error(`none request handle found on ${typeof handler}`);
+        return router;
+    }
+
+    for (const item of map.values()) {
+        var func: (req: Request) => Promise<any> = Object.getPrototypeOf(handler)[item.func];
+        if (func) {
+            let routeFunc: any = null;
+            if (item.method === "GET") {
+                routeFunc = router.get;
+            } else if (item.method === "POST") {
+                routeFunc = router.post;
+            } else {
+                routeFunc = router.use;
+            }
+            routeFunc = routeFunc.bind(router);
+            if (item.security) {
+                routeFunc(item.path, guardian, process(func, handler));
+            } else {
+                routeFunc(item.path, process(func, handler));
+            }
+        }
+    }
+    return router;
+}
+
+type ConnectFunction = (req: Request, resp: Response, next: any) => void;
+
+
+function process(func: Function, thisArg: any): (req: Request, resp: Response) => Promise<void> {
+    return async (req: Request, resp: Response) => {
+        var args = Object.assign({}, req.query, req.body);
+        let ps = getFunctionParams(func);
+        ps = ps.map(i => args[i]);
+        try {
+            const ret = await func.call(thisArg, ...ps);
+            resp.json(ret);
+        } catch (err) {
+            resp.status(400).json({ err });
+        }
+    }
+}
+
+function getHandlerMetadata(target: any, propertyKey: string): ServiceHandlerInfo {
+    var map: Map<string, ServiceHandlerInfo> = Reflect.getMetadata(service_api_map, target);
+    if (!map) {
+        map = new Map<string, ServiceHandlerInfo>();
+        Reflect.defineMetadata(service_api_map, map, target);
+    }
+
+    if (!map.has(propertyKey)) {
+        const info = { path: "", func: propertyKey, method: "any", security: true }
+        map.set(propertyKey, info)
+    }
+    return map.get(propertyKey)!;
+}
+
+function defineRequestMetadata(path: string, target: any, propertyKey: string, security: boolean, method: string) {
+    const info = getHandlerMetadata(target, propertyKey);
+    info.path = path;
+    info.security = security;
+    info.func = propertyKey;
+    info.method = method;
+};
+
+
+const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+const ARGUMENT_NAMES = /([^\s,]+)/g;
+function getFunctionParams(func: Function) {
+    var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+    var result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+    if (result === null)
+        result = [];
+    return result;
+}
+
+interface ServiceHandlerInfo {
+    path: string;
+    func: string;
+    method: string;
+    security: boolean;
+}
+
+const service_router_token = "SERVICE_ROUTER_TOKEN";
+const service_api_map = "SERVICE_API_MAP";
+
